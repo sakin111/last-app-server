f92340f9f9dc45ff0e1ab6a9584c94cbebdfea4b
sakin111 <maliksakin53@gmail.com>
2025-12-13 13:20:40 +0600

    yes

--- prisma/schema/migrations/20251202174326_add_images_to_travel_plan/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "travelPlan" ADD COLUMN     "images" TEXT[];
--- prisma/schema/migrations/20251212115930_review_changes/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `comment` on the `reviews` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "reviews" DROP COLUMN "comment",
-ADD COLUMN     "content" TEXT;
